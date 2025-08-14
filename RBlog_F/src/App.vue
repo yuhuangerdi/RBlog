7da55b5a513700cf66ce206ae0cc46bcbdd817e6
Ryan <3043027043@qq.com>
2025-08-14 19:49:12 +0800

    Frontend

---
+++ RBlog_F/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+    <GoTop></GoTop>
+    <div></div>
+</template>
+
+<script lang="ts">
+    export default {
+        name: 'App'
+    }
+</script>
+
+<script setup lang="ts">
+    
+</script>
+
+<style scoped>
+    div{
+        width: 100px;
+        height: 3000px;
+        background-color: rgb(0, 140, 255);
+    }
+</style>
